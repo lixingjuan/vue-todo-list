594e7f45c30a3a30b38dc1a2d67c3a704f1e61d4
25037 <xingjuan.li@hand-china.com>
2020-04-25 22:14:40 +0800

    feat:deploy脚本

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:lixingjuan/vue-todo-list.git master:gh-pages
+git push -f git@github.com:lixingjuan/vue-todo-list.git master
 
 cd -
