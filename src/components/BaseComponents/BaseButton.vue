ceeac00bd55e91104f8b664cf87aa71773d89dbe
25037 <xingjuan.li@hand-china.com>
2020-04-28 10:06:15 +0800

    feat:添加彩虹装饰

--- src/components/BaseComponents/BaseButton.vue
+++ src/components/BaseComponents/BaseButton.vue
@@ -33,25 +33,24 @@ export default {
 
 <style scoped lang="scss">
 #button {
-  height: 30px;
-  border: 1px solid transparent;
-  display: flexbox;
-  position: relative;
+  height: 40px;
+  color: #fff;
+  padding: 4px 15px;
+  cursor: pointer;
+  font-size: 14px;
   text-align: center;
   box-shadow: 0 2px 0 rgba(0, 0, 0, 0.015);
   font-weight: 400;
   white-space: nowrap;
-  background-image: none;
-  cursor: pointer;
   transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
   user-select: none;
   touch-action: manipulation;
-  padding: 4px 15px;
-  font-size: 14px;
-  border-radius: 2px;
-  color: #fff;
   background-color: #f3d52c;
-  border-color: #ec8d11;
+  border: {
+    border: 2px solid transparent;
+    radius: 3px;
+    color: #ec8d11;
+  }
   span {
     text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.12);
   }
